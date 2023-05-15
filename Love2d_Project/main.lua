5d1cf64839efee66ea8d7732d8b7819bded2bfcc
Connor Fricke <113947184+FXDuke@users.noreply.github.com>
2023-05-15 19:45:21 -0400

    forgot to remove test

--- Love2d_Project/main.lua
+++ Love2d_Project/main.lua
@@ -49,46 +49,8 @@ Instance = require("Modules/instance");
 Tween = require("Modules/tween");
 TweenInfo = Tween.TweenInfo;
 
-local Holder = Instance.new("Folder");
-Holder.Name = "Object__Holder";
 
-local l__Object = Instance.new("Object");
-l__Object.Name = "MyObject";
-l__Object.Parent = Holder;
-l__Object.Destroying:Connect(function()
-    print(tostring(l__Object) .. " DESTROYED!");
-end)
 
-l__Object.Position = Vector2.new(math.random(1,10),math.random(1,10));
-local startingPos = l__Object.Position;
-local goal = Vector2.new(100,100);
-
-Holder.MyObject.Changed:Connect(function(self,Value)
-    if Value == "Position" then 
-        if Holder.MyObject.Position == goal then 
-            print(tostring(Holder.MyObject) .." Goal reached! " .. tostring(Holder.MyObject.Position));
-            print("Distance Moved: " .. tostring((goal-startingPos).Unit*(goal-startingPos).Magnitude) .. "\t(START:" .. tostring(startingPos) .. ")");
-            self:Disconnect();
-        else
-            print(tostring(Holder.MyObject) .. " Moving to goal! " .. tostring(Holder.MyObject.Position))
-        end
-    end
-end)
-
-local MyTween = Tween:Create(Holder.MyObject,TweenInfo.new(2,Enumerate.EasingStyle.Sine),{Position=goal});
-MyTween:Play();
-
-thread(function(self)
-    local Start = os.clock();
-    MyTween.Completed:Wait(self);
-    print("tween completed in " .. os.clock()-Start);
-end):Play();
-
-while #task.Threads>0 do 
-    task.Update();
-end
-
---[[
 function love.draw()
     love.graphics.print("hello world",100,100)
 end
@@ -96,5 +58,3 @@ end
 function love.update(DeltaTime)
     task.Update();
 end
-
-]]
