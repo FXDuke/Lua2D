d46aafec5169f171767e97f11740c4227bfdae34
Connor Fricke <113947184+FXDuke@users.noreply.github.com>
2023-05-15 19:49:31 -0400

    Create enumerate.lua

---
+++ Love2d_Project/Modules/enumerate.lua
@@ -0,0 +1,19 @@
+local Enumerate = {
+    EasingStyle = {
+        Linear = 0,
+        Sine = 1,
+    },
+    EasingDirection = {
+        In = 0,
+        Out = 1,
+        InOut = 2,
+    },
+    PlaybackState = {
+        Normal = 0,
+        Playing = 1,
+        Paused = 2,
+        Completed = 3,
+    };
+};
+
+return Enumerate;
