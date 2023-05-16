686d490d508fac2dd755b302779077d7a8608454
Connor Fricke <113947184+FXDuke@users.noreply.github.com>
2023-05-15 21:41:01 -0400

    forgot to remove notes again lol

--- Love2d_Project/main.lua
+++ Love2d_Project/main.lua
@@ -42,23 +42,6 @@ TweenInfo = Tween.TweenInfo;
 
 UI_Drawing = require("Modules/ui_drawing");
 
-local MyText = Instance.new("TextBox");
-MyText.Text = "Hello world!";
-MyText.Position = UDim2.new(0.5,0,0.5,0);
-MyText.Size = UDim2.new(0.2,0,0.2,0);
-
-thread(function(self)
-    local BecomeTransparent = Tween:Create(MyText,TweenInfo.new(1,Enumerate.EasingStyle.Sine,Enumerate.EasingDirection.InOut),{Position = UDim2.new(0,0,0,0),BackgroundOpacity=0});
-    MyText.Text = "Fading out...";
-    BecomeTransparent:Play();
-    BecomeTransparent.Completed:Wait(self);
-    MyText.Text = "Fading in...";
-    local BecomeNonTransparent = Tween:Create(MyText,TweenInfo.new(1,Enumerate.EasingStyle.Sine,Enumerate.EasingDirection.InOut),{Position = UDim2.new(0.5,0,0.5,0),BackgroundOpacity=1});
-    BecomeNonTransparent:Play();
-    BecomeNonTransparent.Completed:Wait(self);
-end):Loop();
-
-love.graphics.setBackgroundColor(0,0,0);
 function love.draw()
     for _,l__Instance in pairs(Instance.getInstances()) do 
         if l__Instance.Class == "UI" then
