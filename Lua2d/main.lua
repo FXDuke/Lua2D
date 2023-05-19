99ec6d1062b03c860421ce20f159a9adb20d10f3
Connor Fricke <113947184+FXDuke@users.noreply.github.com>
2023-05-19 15:05:44 -0400

    Notes

--- Lua2d/main.lua
+++ Lua2d/main.lua
@@ -8,7 +8,9 @@ Print = print;
 Type = type;
 
 table_to_string = require("Functions/table_to_string");
+-- Temporary debug tool used to print out tables and their contents 
 type = require("Functions/type");
+-- returns the custom type or standard type
 
 print = function(...)
     for _,Data in pairs({...}) do 
@@ -21,30 +23,43 @@ print = function(...)
 end;
 
 task = require("Modules/task");
+-- Task scheduler that runs with love.update. 
+-- Thread passes self as an argument when resumed which can be used to wait, close, or pause the thread
 thread = task.new;
 
 TYPES = require("Modules/types");
+-- Module for the types that contains factories and behaviors 
 Vector2 = TYPES.Vector2;
 Color3 = TYPES.Color3;
 UDim = TYPES.UDim;
 UDim2 = TYPES.UDim2;
 
 connections = require("Modules/connection");
+-- Creates reusable events that can be binded to or fired from any position in the program
 
 createConnection = connections.createConnection;
+-- Creates a connection. If argument 1 (ID) is not specified it is given a numerical value based on the index of connections created
 getConnection = connections.getConnection;
+-- Returns the connection with the specified ID or nil
 waitForConnection = connections.waitForConnection;
+-- Waits for a connection with the specified ID
 
 Instance = require("Modules/instance");
+-- Module for containing the Instance factories as well as instance behavior
 
 Tween = require("Modules/tween");
+-- Uses a dictionary to cast values to the passed object and smoothly animates them to their goals
 TweenInfo = Tween.TweenInfo;
 
 game = Instance.new("Folder");
+-- Game will be a hub for services and events
 game.Name = "game";
 workspace = Instance.new("Folder");
+-- workspace is the folder that holds all the objects that will be put into the physical world
 workspace.Name = "workspace"
+workspace.Parent = game;
 ui_service = Instance.new("UI");
+-- ui_service is a UI Instance that acts as a folder and hold the objects that will be drawn 
 ui_service.Parent = game;
 ui_service.Name = "ui_service"
 ui_service.ScaleType = Enumerate.ScaleType.Global;
@@ -52,20 +67,21 @@ ui_service.ZIndex = 0;
 ui_service.Size = UDim2.new(1,0,1,0);
 ui_service.Position = UDim2.new(0,0,0,0);
 
-workspace.Parent = game;
-
 STUDIO_UI = require("Modules/studio_ui");
+-- The framework for what will be the UI for explorer and editor the workspace and ui worlds
 
 UI_Drawing = require("Modules/ui_drawing");
+-- Provides functions to draw types of instances under the UI Class
 
 Mouse = require("Modules/mouse");
+-- Mouse provides events for mouse behavior, as well as numerical values for mouse attributes
 
 love.graphics.setBackgroundColor(1,1,1);
-
 function love.draw()
     for _,ZIndex in pairs(Instance.getDrawOrder()) do 
+        -- Draw Order is a table updated every time an instance under the UI Class's ZIndex changes
         for _,l__Instance in ipairs(ZIndex) do 
-            if l__Instance.Class == "UI" then -- need to add zindex 
+            if l__Instance.Class == "UI" then
                 if l__Instance.__Attributes.Enabled and l__Instance.__Attributes.Visible then 
                     UI_Drawing[l__Instance.Type](l__Instance);
                 end
@@ -81,6 +97,7 @@ function love.mousemoved(X,Y)
             local Position = UIObject.AbsolutePosition;
             local Size = UIObject.AbsoluteSize;
             local Boundary = (X >= Position.X and X <= Position.X+Size.X) and (Y >= Position.Y and Y <= Position.Y+Size.Y);
+            -- Boundary is a boolean that checks if the mouse is inside of the UI 
             if Boundary and Mouse.Hit ~= UIObject and UIObject.ZIndex >= Mouse.Hit.ZIndex then 
                 Mouse.Hit.MouseLeave:Fire();
                 Mouse.Hit = UIObject;
@@ -97,7 +114,7 @@ end
 
 function love.mousepressed(X,Y,Button)
     if Button == 1 then 
-        if Mouse.Hit.Type == "Button" then 
+        if Mouse.Hit.Type == "Button" then
             Mouse.Hit.Button1Down:Fire();
         end
         Mouse.Button1Down:Fire();
